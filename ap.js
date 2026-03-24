71c11fe58917bd37f88d4efb94eeac80e342d268
muddassir <mhammadmuddassir587@gmail.com>
2026-03-24 22:13:37 +0845

    first-commit

---
+++ ap.js
@@ -0,0 +1,91 @@
+
+
+//  let age = 25;
+//  let price = 99.5;
+
+//  console.log(price);
+
+// let fullName = "tony star"
+
+// console.log(fullName)
+
+
+// const student = {
+//     name: "muddassir",
+//     class: 10,
+//     age: 20,
+//     he: "boy",
+//     ispass: true,
+// };
+
+// student["name"] = "Muddassir Shahzad";
+
+// console.log(student);
+
+
+
+// const product = {
+//     tittle: "pen",
+//     ratting: 4,
+//     offer: 5,
+//     price: 220
+// };
+// console.log(product);
+
+
+// const profile = {
+//     username: "@Muddassir shahzad",
+//     isFollow: false,
+//     followers: 222,
+//     following: 123,
+// };
+// console.log(typeof profile["followers"]);
+ 
+
+// let a = 10
+// let b = 10
+
+// console.log("a =", a, "& b =", b);
+// console.log("a+b", a + b);
+// console.log("a-b", a - b);
+// console.log("a*b", a * b);
+// console.log("a/b", a / b);
+
+// let a = 5
+// let b = 2
+
+// console.log("a =", a, " & b =", b);
+// // a++;
+// console.log("a++ =", a++);
+// console.log("a = " ,a);
+// console.log("a = " ,a);
+
+// let a = 5
+// let b  = 2
+
+// a /= 4;
+//  console.log("a = ",a);
+
+
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// if (mode === "light") {
+//     color = "white";
+// }
+// console.log(color);
+
+
+// if  (age >= 18) {
+//     console.log("you can vote");
+//   }
+
+
+// if (age < 18) {
+//   console.log("you cannot vote");
+console.log 
---
+++ jawa.js
@@ -0,0 +1,52 @@
+const products = [
+    {
+        id: 1,
+        name: "Wireless Mouse",
+        price: 1200,
+        category: "Electronics",
+        stock: 25,
+        image: "mouse.jpg"
+    },
+    {
+        id: 2,
+        name: "Bluetooth Headphones",
+        price: 3500,
+        category: "Electronics",
+        stock: 15,
+        image: "headphones.jpg"
+    },
+    {
+        id: 3,
+        name: "Men's T-Shirt",
+        price: 900,
+        category: "Clothing",
+        stock: 40,
+        image: "tshirt.jpg"
+    },
+    {
+        id: 4,
+        name: "Running Shoes",
+        price: 4500,
+        category: "Footwear",
+        stock: 20,
+        image: "shoes.jpg"
+    },
+    {
+        id: 5,
+        name: "Smart Watch",
+        price: 7000,
+        category: "Electronics",
+        stock: 10,
+        image: "smartwatch.jpg"
+    },
+    {
+        id: 6,
+        name: "Backpack",
+        price: 1800,
+        category: "Accessories",
+        stock: 30,
+        image: "backpack.jpg"
+    }
+];
+
+const products
